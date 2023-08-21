c50f298361ccc657b50e4567ac62feb0a5ef2125
sagit <sagitgaysin87@gmail.com>
2023-08-21 16:22:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        //task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
+        //task9();
+        //task10();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        //Условие задачи: С помощью цикла for выведите в консоль все числа от 1 до 10.
+
+        //Решение задачи:
+        for (int i = 0; i <= 10; i = i + 1) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println();
+    }
+}
